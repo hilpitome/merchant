a37849b61609a2649b8042143262932fd9571774
hilary <hilpitome@gmail.com>
2016-02-11 22:24:45 +0300

    added stock column; added stock validations

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,20 @@ module ProductsHelper
 	def print_price(price)
      format("Ksh%.2f", price)
     end
+
+    def print_stock(stock)
+    	
+     if stock > 0
+
+     	content_tag(:span, content_tag(:p, "In Stock #{stock}"), class: "in_stock")
+     	
+  	 else
+
+        content_tag(:span, content_tag(:p, "Out of Stock"), class: "out_stock")
+
+     end
+
+    end
+
 end
+
