0c7a58f8607dd03135085e619e92c15b61b8b65a
hilary <hilpitome@gmail.com>
2016-02-12 23:38:12 +0300

    designed orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
+	has_many :order_items
 	validates_numericality_of :price
 	validates_numericality_of :stock, only_integer: true, greater_than: 0
+
 	def price=(input)
     	input.delete!("Kes")
     super
