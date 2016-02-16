9f4e552032938307a73148f2a4b27aae7e59e6f4
hilary <hilpitome@gmail.com>
2016-02-16 23:52:53 +0300

    modified order show page

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,10 @@ class OrderItem < ActiveRecord::Base
 	belongs_to :order
 	
 	belongs_to :product
-	
+
+	def subtotal(quantity, price)
+	 
+	  price * quantity
+	 
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	has_many :order_items
-	validates_numericality_of :price
+	validates_numericality_of :price, :presence => true
 	validates_numericality_of :stock, only_integer: true, greater_than: 0
 
 	def price=(input)
