a709df59d0419f213e32dedf45e24657f9fa3174
hilary <hilpitome@gmail.com>
2016-02-22 22:20:14 +0300

    made stock checking more intelligent

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,15 +5,20 @@ module ProductsHelper
     
     end
 
-    def print_stock(stock)
+    def print_stock(stock, requested = 1)
     	
-     if stock > 0
-
-     	content_tag(:span, content_tag(:p, "In Stock #{stock}"), class: "in_stock")
-     	
-  	 else
+     if stock == 0
 
         content_tag(:span, content_tag(:p, "Out of Stock"), class: "out_stock")
+
+     	
+  	 elsif stock > 0 && requested < stock 
+
+        content_tag(:span, content_tag(:p, "In Stock #{stock}"), class: "in_stock")
+
+     else
+
+       content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock") 
 
      end
 
